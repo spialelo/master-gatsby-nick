11017afa7780b18340f64e92c835f38bb6675df0
spialelo <ndphillips@gmail.com>
2022-10-30 00:23:31 -0700

    additional nav styles plus logo styles added

--- starter-files/gatsby/src/components/Nav.js
+++ starter-files/gatsby/src/components/Nav.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
+import Logo from './Logo';
 
 const NavStyles = styled.nav`
-  background: green;
+  margine-bottom: 3rem;
+  .log {
+    transform: translateY(-25%);
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    test-align: center;
+    list-style: none;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+    align-items: center;
+  }
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+    &:nth-child(2) {
+      --rotate: -2.5deg;
+    }
+    &:nth-child(4) {
+      --rotate: 2.5deg;
+    }
+    &:hover {
+      --rotate: 3deg;
+    }
+  }
   a {
-    background: red;
+    font-size: 3rem;
+    text-decoration: none;
+    &:hover {
+      color: var(--red);
+    }
   }
 `;
 
@@ -20,13 +56,15 @@ export default function Nav() {
           <Link to="/pizzas">Pizza Menu</Link>
         </li>
         <li>
-          <Link to="/">LOGO</Link>
+          <Link to="/">
+            <Logo />
+          </Link>
         </li>
         <li>
-          <Link to="/slicemasters">Pizzas</Link>
+          <Link to="/slicemasters">SliceMasters</Link>
         </li>
         <li>
-          <Link to="/order">Order</Link>
+          <Link to="/order">Order Ahead!</Link>
         </li>
       </ul>
     </NavStyles>
