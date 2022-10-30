06c4d510196405c74c7382af485b84c62962cf41
spialelo <ndphillips@gmail.com>
2022-10-30 00:41:39 -0700

    stripe styling and more examples of clamp usage

--- starter-files/gatsby/src/components/Layout.js
+++ starter-files/gatsby/src/components/Layout.js
@@ -1,18 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
 import Footer from './Footer';
 import Nav from './Nav';
 import 'normalize.css';
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
+import stripes from '../assets/images/stripes.svg';
+
+const SiteBorderStyles = styled.div`
+  max-width: 1000px;
+  margin: 12rem auto 4rem auto;
+  margin-top: clamp(2rem, 10vw, 12rem);
+  background: white url(${stripes});
+  padding: 5px;
+  padding: clamp(5px, 1vw, 25px);
+`;
+
+const ContentStyles = styled.div`
+  display: grid;
+  background: white;
+  padding: 2rem;
+`;
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <>
       <GlobalStyles />
       <Typography />
-      <Nav />
-      {children}
-      <Footer />
-    </div>
+      <SiteBorderStyles>
+        <ContentStyles>
+          <Nav />
+          {children}
+          <Footer />
+        </ContentStyles>
+      </SiteBorderStyles>
+    </>
   );
 }
