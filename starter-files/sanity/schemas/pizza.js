f99a3becdfdb2025535ebda933f85a7485b640d8
spialelo <ndphillips@gmail.com>
2022-11-03 21:57:08 -0700

    initial schema commit

---
+++ starter-files/sanity/schemas/pizza.js
@@ -0,0 +1,15 @@
+export default {
+  // computer name
+  name: 'pizza',
+  // visible title
+  title: 'Pizzas',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'Name of the pizza',
+    },
+  ],
+};
